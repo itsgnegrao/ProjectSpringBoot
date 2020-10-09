5a29b56b06ac491a2e1d89999e8050cbbaddf799
itsgnegrao <itsg_negrao@hotmail.com>
2020-10-09 01:18:25 -0300

    malfeito feito!

--- src/main/java/itsgnegrao/ProjectSpringBoot/resources/SourceResource.java
+++ src/main/java/itsgnegrao/ProjectSpringBoot/resources/SourceResource.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static itsgnegrao.ProjectSpringBoot.configs.ConfigsFrontEnd.API_URL;
-import static itsgnegrao.ProjectSpringBoot.configs.ConfigsFrontEnd.API_URL_LOCAL;
 
 @RestController
 @RequestMapping("/source")
@@ -19,11 +17,11 @@ public class SourceResource {
                 " <br/><br/>" +
                 "<a href=\"https://github.com/itsgnegrao/ProjectReact\"> 2. Link do Projeto Front-End ReactJs no Github. <a> " +
                 " <br/><br/>" +
-                "<a href=\"https://hub.docker.com/r/itsgnegrao/itsgnegrao-spring-boot\"> 3. Link do Projeto Front-End ReactJs no Docker Hub. <a> "+
+                "<a href=\"https://hub.docker.com/r/itsgnegrao/itsgnegrao-spring-boot\"> 3. Link do Projeto Back-End Java Spring Boot no Docker Hub. <a> " +
                 " <br/><br/>" +
                 "<a href=\"https://hub.docker.com/repository/docker/itsgnegrao/itsgnegrao-react-app\"> 4. Link do Projeto Front-End ReactJs no Docker Hub. <a> " +
                 " <br/><br/>" +
-                "<a href=\"https://itsgnegrao-spring-boot.herokuapp.com/\"> 5. Link do Projeto Front-End ReactJs no Heroku. <a> " +
+                "<a href=\"https://itsgnegrao-spring-boot.herokuapp.com/\"> 5. Link do Projeto Back-End Java Spring Boot no Heroku. <a> " +
                 " <br/><br/>" +
                 "<a href=\"https://itsgnegrao-react-app.herokuapp.com/\"> 6. Link do Projeto Front-End ReactJs no Heroku. <a> ";
     }
