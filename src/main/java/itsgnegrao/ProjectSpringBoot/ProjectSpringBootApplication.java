051aa7490388db1cf3f5aecb83898ee3236ab45e
itsgnegrao <itsg_negrao@hotmail.com>
2020-10-09 00:08:58 -0300

    and now

--- src/main/java/itsgnegrao/ProjectSpringBoot/ProjectSpringBootApplication.java
+++ src/main/java/itsgnegrao/ProjectSpringBoot/ProjectSpringBootApplication.java
@@ -7,8 +7,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 import static itsgnegrao.ProjectSpringBoot.configs.ConfigsFrontEnd.API_URL;
 import static itsgnegrao.ProjectSpringBoot.configs.ConfigsFrontEnd.API_URL_LOCAL;
@@ -35,6 +40,19 @@ public class ProjectSpringBootApplication extends SpringBootServletInitializer {
                 registry.addMapping("/api/client/**").allowedOrigins(API_URL_LOCAL, API_URL);
             }
         };
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(API_URL_LOCAL, API_URL));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
